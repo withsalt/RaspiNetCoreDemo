20191c8155fd58d2a96854478aee96b6f4ef39f8
withsalt <sysrun@163.com>
2019-09-23 14:45:27 +0800

    添加SPI和PWM

---
+++ src/6.PWM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Device.Pwm.Drivers;
+using System.Threading;
+
+namespace _6.PWM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello PWM!");
+            try
+            {
+                using (var pwmChannel = new SoftwarePwmChannel(17))
+                {
+                    pwmChannel.Start();
+                    for (double fill = 0.0; fill <= 1.0; fill += 0.01)
+                    {
+                        pwmChannel.DutyCyclePercentage = fill;
+                        Thread.Sleep(300);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
