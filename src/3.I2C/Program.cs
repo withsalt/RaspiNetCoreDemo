c0ed64c35e9056db0818f21915687526bb90a739
withsalt <sysrun@163.com>
2019-09-23 12:49:29 +0800

    添加IIC和UART

---
+++ src/3.I2C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3.I2C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/4.UART/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+
+namespace _4.UART
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Serial port!");
+            //注册退出事件
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs eventArgs) => { };
+
+            var ports = SerialPort.GetPortNames();
+            bool isTTY = false;
+            foreach (var prt in ports)
+            {
+                Console.WriteLine($"Serial name: {prt}");
+                if (prt.Contains("ttyS0"))
+                {
+                    isTTY = true;
+                }
+            }
+            if (!isTTY)
+            {
+                Console.WriteLine("No ttyS0 serial port!");
+                return;
+            }
+            Console.WriteLine("Yes, we have the embedded serial port available, opening it");
+            SerialPort mySer = new SerialPort("/dev/ttyS0", 115200);
+            mySer.DataReceived += DataReceived;
+            mySer.Open();
+            while (true)
+            {
+                byte[] send = Encoding.UTF8.GetBytes("NOW:" + DateTime.Now.ToString());
+                mySer.Write(send, 0, send.Length);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine($"Received: {e.EventType.ToString()}");
+        }
+    }
+}
